f86430bdc8051b1e26df68ee852a595bff5e074d
gurjeetsinghvirdee <gurjeetsinghvirdee@gmail.com>
2021-05-28 18:35:58 +0530

    maked a recipe model

--- src/app/Mycomponents/recipes/recipe-list/recipe-list.component.ts
+++ src/app/Mycomponents/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe("Paneer Butter", "This dish would be made with a few large slabs",
+    "https://assets.epicurious.com/photos/5df7c9efa9f50600094dffed/6:4/w_274%2Ch_169/COOK90_ButterPaneer_RECIPE_V2_121119_288.jpg"),
+    new Recipe("Paneer Butter", "This dish would be made with a few large slabs",
+    "https://assets.epicurious.com/photos/5df7c9efa9f50600094dffed/6:4/w_274%2Ch_169/COOK90_ButterPaneer_RECIPE_V2_121119_288.jpg")
+  ]
   constructor() { }
 
   ngOnInit(): void {
